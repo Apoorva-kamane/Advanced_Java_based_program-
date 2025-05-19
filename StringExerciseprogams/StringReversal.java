472dbe7e17b16e3b4291156f6482133a2998e293
Apoorva-kamane <apoorvapkamane@gmail.com>
2025-05-19 21:45:07 +0530

    Add files via upload

---
+++ StringExerciseprogams/StringReversal.java
@@ -0,0 +1,25 @@
+package lab_3;
+import java.util.Scanner;
+public class StringReversal {
+	    // User-defined function to reverse a string
+	    public static String reverseString(String str) {
+	        StringBuilder reversed = new StringBuilder(str);
+	        return reversed.reverse().toString();
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        // Get string input from user
+	        System.out.print("Enter a string to reverse: ");
+	        String input = scanner.nextLine();
+
+	        // Call the reverse function
+	        String reversed = reverseString(input);
+	        System.out.println("Reversed string: " + reversed);
+
+	        scanner.close();
+	    }
+	
+
+}
