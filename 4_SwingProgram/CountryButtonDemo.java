9aa176004dcb8e145770a79ba14d22df5075e7ac
Apoorva-kamane <apoorvapkamane@gmail.com>
2025-05-21 19:25:14 +0530

    Update CountryButtonDemo.java

--- 4_SwingProgram/CountryButtonDemo.java
+++ 4_SwingProgram/CountryButtonDemo.java
@@ -1,5 +1,6 @@
-/*Basic hello program of Swing displaying the message Hello! VI A , Welcome to Swing
-Programming in Blue color plain font with font size of 32 using Jframe and Jlabel*/
+/*Develop a Swing program in Java to display a message “Srilanka is pressed” or “India is
+pressed” depending upon the Jbutton either Srilanka or India is pressed by implementing the
+event handling mechanism with addActionListener( ).*/
 
 package lab_4;
 import javax.swing.*;
