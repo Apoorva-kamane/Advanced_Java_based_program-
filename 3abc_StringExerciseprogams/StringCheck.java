eb847b257ef8f8a757c4314d4041009d9062410b
Apoorva-kamane <apoorvapkamane@gmail.com>
2025-05-21 19:12:31 +0530

    Update StringCheck.java

--- 3abc_StringExerciseprogams/StringCheck.java
+++ 3abc_StringExerciseprogams/StringCheck.java
@@ -1,3 +1,6 @@
+/*Write a Java Program for Checking if a given string is null or contains only whitespace using user
+defined function isNullOrEmpty().*/
+
 package lab_3;
 import java.util.Scanner;
 public class StringCheck {
